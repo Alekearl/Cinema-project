f2eee1523ce79da23aacd6ec7d120076a76cac87
Alekearl <alexandrhurko@gmail.com>
2021-02-04 11:36:18 +0200

    Changes according to advises.

--- src/main/java/com/dev/cinema/model/ShoppingCart.java
+++ src/main/java/com/dev/cinema/model/ShoppingCart.java
@@ -2,10 +2,7 @@ package com.dev.cinema.model;
 
 import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
