b4dccde25c41c0dcb2a2a27b2a584b60a984e861
Alekearl <alexandrhurko@gmail.com>
2021-02-17 11:48:32 +0200

    Removed some not used fields and methods according to YAGNI

--- src/main/java/com/dev/cinema/service/mapper/UserMapper.java
+++ src/main/java/com/dev/cinema/service/mapper/UserMapper.java
@@ -1,7 +1,6 @@
 package com.dev.cinema.service.mapper;
 
 import com.dev.cinema.model.User;
-import com.dev.cinema.model.dto.UserDtoRequest;
 import com.dev.cinema.model.dto.UserDtoResponse;
 import org.springframework.stereotype.Component;
 
@@ -12,12 +11,5 @@ public class UserMapper {
         response.setId(user.getId());
         response.setEmail(user.getEmail());
         return response;
-    }
-
-    public User mapFromDto(UserDtoRequest request) {
-        User user = new User();
-        user.setEmail(request.getEmail());
-        user.setPassword(request.getPassword());
-        return user;
     }
 }
